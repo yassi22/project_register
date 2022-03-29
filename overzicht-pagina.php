ecc379c726c7c7975705afd0b30b1409e6ee07a6
Yassin Chamlal <yassin-c02@hotmail.com>
2022-03-29 11:37:06 +0200

    Overzicht knop toegevoegd

--- overzicht-pagina.php
+++ overzicht-pagina.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> 
+  <link rel="stylesheet" href="style.css">
   <style>
   .fakeimg {
     height: 200px;
@@ -26,8 +27,8 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li class="nav-item"> 
+      <button class="button button5 w-6">Nieuw project toevoegen</button>
       </li>
     </ul>
   </div>
