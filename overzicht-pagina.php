f21250ef37b62b0d9a5b8b8c84022840c89ed654
Yassin Chamlal <yassin-c02@hotmail.com>
2022-03-31 15:33:36 +0200

    Detial knop toegevoegd

--- overzicht-pagina.php
+++ overzicht-pagina.php
@@ -156,7 +156,8 @@
     <img src="img/DDDonline.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
-      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> 
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
@@ -165,7 +166,8 @@
     <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
-      <p class="card-text omschrijf">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text omschrijf">This card has supporting text below as a natural lead-in to additional content.</p>  
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
@@ -173,7 +175,8 @@
     <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
-      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> 
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
@@ -184,7 +187,8 @@
     <img src="img/DDDonline.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
-      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> 
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
@@ -194,6 +198,7 @@
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
       <p class="card-text omschrijf">This card has supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
@@ -201,7 +206,8 @@
     <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title titel">Card title</h5>
-      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text omschrijf">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> 
+      <a href="#" class="btn knopdetail">Meer detials</a>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
