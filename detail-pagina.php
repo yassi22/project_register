7b9f50f74353b3f0f20d17362106b338aef948bb
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-01 14:30:09 +0200

    New layout

--- detail-pagina.php
+++ detail-pagina.php
@@ -7,6 +7,12 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> 
   <link rel="stylesheet" href="style.css">
+  
+  <!-- Load Google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  
   <style>
   .fakeimg {
     height: 200px;
@@ -25,7 +31,7 @@
       </a>
       
        
-      <a href="overzicht-pagina.php" class="btn btn-lg knop d-flex justify-content-end" role="button">Nieuw project toevoegen</a>
+      <a href="overzicht-pagina.php" class="btn btn-lg knop d-flex justify-content-end" role="button">Project toevoegen</a>
  
      
   </div>
@@ -37,21 +43,21 @@
 <div class="container-fluid  "> 
   
   <div class="row"> 
-    <div class="col-md-4 d-flex flex-row-reverse ">
-    <div class="col-sm-4">
-      <h2>Filteren</h2>
-      <h5>Jaartal</h5>
-      <ul> 
+    <div class="col-md-3 d-flex justify-content-end margin-right-2">
+    <div class="col-sm-6 verplaats">
+      <h2>Filteren</h2> 
+      <h5 class="text-center">Jaartal</h5> 
       
-<div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-  <label class="form-check-label" for="flexSwitchCheckDefault">    <li> 2017  </li> </label>
-</div> 
-
-       <div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-  <label class="form-check-label" for="flexSwitchCheckDefault">   <li class="text-center"> 2018 </li> </label>
-</div> 
+      <ul> 
+        <div class="form-check  form-switch">
+          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+          <label class="form-check-label" for="flexSwitchCheckDefault">    <li> 2017  </li> </label>
+        </div> 
+ 
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+          <label class="form-check-label" for="flexSwitchCheckDefault">   <li class="text-center"> 2018 </li> </label>
+        </div> 
 
       <div class="form-check form-switch">
   <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
@@ -70,14 +76,14 @@
         <div class="form-check form-switch">
   <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
   <label class="form-check-label" for="flexSwitchCheckDefault">    <li> 2022 </li></label>
-</div> 
-
+</div>
+ 
       </ul>   
 
       <hr>  
 
-    
-      <h5>Diensten</h5>
+     
+    <h5 class="text-center">Diensten</h5>
       <ul> 
    
 <div class="form-check form-switch">
@@ -97,12 +103,12 @@
 
   <div class="form-check form-switch">
   <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-  <label class="form-check-label" for="flexSwitchCheckDefault"> <li> Uitnodigings trajcet </li> </label>
+  <label class="form-check-label" for="flexSwitchCheckDefault"> <li> Uitnodigingstrajcet </li> </label>
 </div> 
 
     <div class="form-check form-switch">
   <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-  <label class="form-check-label" for="flexSwitchCheckDefault"> <li> Congress online </li> </label>
+  <label class="form-check-label" for="flexSwitchCheckDefault"> <li> Congres online </li> </label>
 </div>  
 
     
@@ -116,7 +122,7 @@
  
 <hr> 
 
-      <h5>Categorie</h5>
+      <h5 class="text-center">Categorie</h5>
       <ul> 
   
 <div class="form-check form-switch">
@@ -135,7 +141,7 @@
   <label class="form-check-label" for="flexSwitchCheckDefault"> <li> Congress online eventen </li> </label> 
 
 </div>  
-<button type="button" class="btn btn zoekknop mt-4 mx-auto " type="submit">Resultaat</button>
+<button type="button" class="btn mt-4 text-center resultaat-knop " type="submit">Resultaat</button>
   </ul>    
   
 
@@ -143,80 +149,77 @@
 </div>
  
  
-  <div class="col-md-6"> 
-     
-    <h2>Projecten</h2>      
-    
-    <div class="float-right">  
-    <form class="form-inline d-flex justify-end">
-    <input class="form-control zoek mr-sm-2 w-0" type="text" placeholder="Search"> 
-    <button class="btn my-2 my-sm-0 zoekknop" type="submit">Search</button>
-  </form>
+  <div class="col-md-8"> 
+    <div class="float-right verplaats-cards">   
+      <section class="d-flex align-items-center justify-content-between"> 
+        <h2>Projecten</h2> 
+        <div class="zoek-verplaats"> 
+          <form class=" d-flex"> 
+            <input class="form-control zoek mr-sm-2" type="text" placeholder="Search"> 
+            <button class="btn my-2 my-sm-0 zoekknop " type="submit">Search</button>
+          </form> 
+        </div>
+      </section>
+ 
 <div>
-    <div class="card-group m-2 ">
+    <div class="card-group m-6 ">
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/Hoek&Blok-logo.png" class="card-img-top embed-responsive-item rounded" alt="schermafbeelding Hoek en Blok Personeelsevent">
+    <div class="card-body project-card">
+      <h5 class="card-title titel">Hoek en Blok Personeelsevent</h5> 
+     <p class="card-text omschrijf">  8-01-21 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
     </div>
   </div>
   
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/ECSJ-logo.png"  class="card-img-top rounded embed-responsive-item  " alt="scherm-afbeelding ECSJ">
+    <div class="card-body project-card">
+    <h5 class="card-title titel">ECSJ Leiden</h5>
+     <p class="card-text omschrijf">  10-07-22 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
+      
     </div>
-  </div>
+  </div> 
+
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/bleckmann-logo.png"  class="card-img-top rounded" alt="scherm-afbeelding Bleckmann 160 jaar">
+    <div class="card-body project-card">
+    <h5 class="card-title titel">Embrace 160 years Bleckmann</h5> 
+     <p class="card-text omschrijf">  29-6-22 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
+      
     </div>
   </div>
 </div> 
 
 <div class="card-group m-2 ">
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/enexis-energie-event.png" class="card-img-top rounded" alt="schermafbeelding Enexis Transition">
+    <div class="card-body project-card">
+    <h5 class="card-title titel">Enexis Transition</h5> 
+     <p class="card-text omschrijf">  10-02-2022 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
+       
     </div>
   </div>
   
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/Eniginia-event.png"  class="card-img-top rounded " alt="schermafbeelding eniginia event">
+    <div class="card-body project-card">
+    <h5 class="card-title titel">Enginia Event 2021</h5> 
+     <p class="card-text omschrijf">  12-01-21 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
+       
     </div>
   </div>
   <div class="card p-2 m-2 shadow p-3 mb-5 bg-white rounded">
-    <img src="img/DDDonline.jpg"  class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title titel">DDD Online</h5> 
-     <p class="card-text omschrijf"> Klantnaam:DDD </p> 
-     <p class="card-text omschrijf"> Datum:2022 </p> 
-      <a href="#" class="btn knopdetail">Meer detials</a>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="img/JA21-logo.png"  class="card-img-top rounded" alt="schermafbeelding JA21">
+    <div class="card-body project-card">
+    <h5 class="card-title titel">JA21 ALV</h5> 
+     <p class="card-text omschrijf"> 21-05-22 </p> 
+      <a href="#" class="btn knopdetail">Meer details</a>
+      
     </div>
   </div>
 </div>
