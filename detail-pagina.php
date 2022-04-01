8b18d2f0856ca416f844caca12d6340f5c23718c
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-01 15:26:57 +0200

    aanpassingen
    jumbo tron toegevoegd

--- detail-pagina.php
+++ detail-pagina.php
@@ -52,14 +52,9 @@
       </section>
 
  <div> 
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
+<div class="jumbotron shadow p-3 mb-5 bg-white rounded d-flex justify-conten-center mt-4">
+   
+  
 </div>
 </div>
 
