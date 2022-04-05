8cd89f7d1cddfe3b37325c013da53c6083e794af
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-05 15:15:56 +0200

    Aanpassingen
    Jumbotron heeft de juiste grote

--- detail-pagina.php
+++ detail-pagina.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Project register - overzicht</title>
+  <title>Project register - detial</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -46,14 +46,13 @@
   <div class="row"> 
 
  
-  <div class="col-md-5"> 
-    
+  
+
+ <div class="col-md-12  justify-content-center   ">  
       <section class="mx-auto"> 
         <h2>Project Details</h2> 
       </section>
-
- <div class="col-md-5 "> 
-<div class="jumbotron shadow p-3 mb-5 bg-white rounded d-flex justify-conten-center mt-4  jumbo-grote"> 
+<div class="jumbotron shadow p-3 mb-5 bg-white rounded d-flex justify-content-center mt-4  jumbo-grote"> 
     <ul>
       <li> 
         <p class="text-secondary">Project naam</p> 
@@ -108,7 +107,7 @@
  
 </div>
 </div>
-</div>
+ 
  
 </body> 
 
