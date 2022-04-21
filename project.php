45053756cb89968e9dc93f52bd7e5064ae611609
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-21 15:55:39 +0200

    code format

--- project.php
+++ project.php
@@ -2,12 +2,12 @@
 require_once 'backend/autoloader.php';
 
 
-     /* 
+/* 
       Hieronder worden projecten op gehaald doormiddel van het project id
-      */  
+      */
 
 $projects = $projectIns->getProject($_GET['id']);
- 
+
 
 ?>
 
@@ -26,9 +26,7 @@ $projects = $projectIns->getProject($_GET['id']);
     <!-- Load Google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
     <style>
         .fakeimg {
@@ -50,10 +48,8 @@ $projects = $projectIns->getProject($_GET['id']);
                 <a href="overzicht-pagina.php" class="btn btn-lg knop d-flex justify-content-end" role="button">Terug
                     naar
                     overzicht</a>
-                <a href="overzicht-pagina.php" class="btn btn-lg d-flex justify-content-end knop-aanpassen"
-                    role="button">Project aanpassen</a>
-                <a href="overzicht-pagina.php" class="btn btn-lg d-flex justify-content-end knop-verwijder"
-                    role="button">Project verwijderen</a>
+                <a href="overzicht-pagina.php" class="btn btn-lg d-flex justify-content-end knop-aanpassen" role="button">Project aanpassen</a>
+                <a href="overzicht-pagina.php" class="btn btn-lg d-flex justify-content-end knop-verwijder" role="button">Project verwijderen</a>
             </div>
         </div>
     </nav>
@@ -80,6 +76,7 @@ $projects = $projectIns->getProject($_GET['id']);
                                 </li>
                                 <li>
                                     <p class="text-secondary mb-0">Website link</p>
+                                    <!--Dit is een comment voorbeeld-->
                                     <p><?php echo $projects->website_link ?></p>
                                 </li>
                                 <li>
@@ -94,8 +91,7 @@ $projects = $projectIns->getProject($_GET['id']);
                         </div>
                         <div class="col col-6 py-4 px-5">
                             <div class="mb-5">
-                                <div id="carouselExampleControls" class="carousel carousel-detail mb-2 slide"
-                                    data-bs-ride="carousel">
+                                <div id="carouselExampleControls" class="carousel carousel-detail mb-2 slide" data-bs-ride="carousel">
                                     <div class="carousel-inner">
                                         <div class="carousel-item  rounded-3 active">
                                             <img src="img/DDDonline.jpg" class="d-block w-100" alt="...">
@@ -107,13 +103,11 @@ $projects = $projectIns->getProject($_GET['id']);
                                             <img src="img/bleckmann-logo.png" class="d-block w-100" alt="...">
                                         </div>
                                     </div>
-                                    <button class="carousel-control-prev" type="button"
-                                        data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                         <span class="visually-hidden">Previous</span>
                                     </button>
-                                    <button class="carousel-control-next" type="button"
-                                        data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                         <span class="visually-hidden">Next</span>
                                     </button>
