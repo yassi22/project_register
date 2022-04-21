e29d5c1e90ddf734ce66d5b9b1d09b02bffcd628
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-21 15:43:42 +0200

    Comments toegevoegd aan class Project

--- backend/class/Project.php
+++ backend/class/Project.php
@@ -16,7 +16,11 @@ class Project extends DbConfig
    @return true|string|void
 */
 
-
+   
+      /* 
+      De methode getAllProjects zorgt er voor dat alles uit de projecten tabel word 
+      gehaald.
+      */   
    public function getAllProjects()
    {
       $sql = "SELECT * FROM projecten";
@@ -25,7 +29,10 @@ class Project extends DbConfig
       return $stmt->fetchAll(PDO::FETCH_OBJ);
    }
 
-
+     /* 
+      De methode getProject zorgt ervoor dat een project met een specefieke id word opgehaald 
+      uit de tabel projecten
+      */  
    public function getProject($id)
    {
       $this->updateView($id);
@@ -37,7 +44,10 @@ class Project extends DbConfig
       return $stmt->fetch(PDO::FETCH_OBJ);
    }
 
-
+     /* 
+      De methode getPopulairProjects zorgt er voor dat projecten
+      met de hoogste aantal views word opgehaald.
+      */  
    public function getPopulairProjects()
    {
       $sql = "SELECT * FROM projecten ORDER BY views DESC";
@@ -46,7 +56,10 @@ class Project extends DbConfig
       return $stmt->fetchAll(PDO::FETCH_OBJ);
    }
 
-
+     /* 
+     De methode getAlhpaProjects zorgt er voor dat projecten  
+     Op Alphabetische volgorde word opgehaald
+      */  
 
    public function getAlphaProjects()
    {
@@ -55,7 +68,11 @@ class Project extends DbConfig
       $stmt->execute();
       return $stmt->fetchAll(PDO::FETCH_OBJ);
    }
-
+    
+     /* 
+     De methode getRecentProjects zorgt er voor dat meest recenten project(wat toegevoegd is)
+     word opgehaald. 
+      */  
    public function getRecentProjects()
    {
       $sql = "SELECT * FROM projecten ORDER BY created_on DESC";
@@ -64,7 +81,10 @@ class Project extends DbConfig
       return $stmt->fetchAll(PDO::FETCH_OBJ);
    }
 
-
+     /* 
+     De methode getDefault zorgt er voor de alle projecten word opgehaald. 
+     Dit is gedaan voor de sorteer onderdeel van de applicatie.
+      */  
    public function getDefault()
    {
       $sql = "SELECT * FROM projecten";
@@ -73,7 +93,10 @@ class Project extends DbConfig
       return $stmt->fetchAll(PDO::FETCH_OBJ);
    }
 
-
+      /* 
+     De methode updateView zorgt er voor dat de kolom views word aangepast waneer een gebruiker 
+     een project bekijkt.
+      */  
    public function updateView($id)
    {
       $jan = "UPDATE projecten set views=(views+1) WHERE project_id =:id";
