bf77a415911392fe981397f2b8bb06543b9d0a06
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-26 15:11:49 +0200

    Revert "Aanpassingen"
    This reverts commit 2f0fa716d0b47f3d1197a1d0cdd04c19079cc08d.

--- JS/image-javascript.js
+++ JS/image-javascript.js
@@ -1,7 +1,4 @@
-
-// Hieronder bevind zich een methode voor het laten zien van een afbeelding. 
-// De afbeelding word opgehaald van uit het bestand syteem van de computer.
-    $(function () {
+$(function () {
     $("input[name=file1]").previewimage({
         div: ".preview",
         imgwidth: 180,
