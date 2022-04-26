2f0fa716d0b47f3d1197a1d0cdd04c19079cc08d
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-26 14:57:13 +0200

    Aanpassingen
    Js comment toegevoegd
    Onnodige code verwijderd

--- JS/image-javascript.js
+++ JS/image-javascript.js
@@ -1,4 +1,7 @@
-$(function () {
+
+// Hieronder bevind zich een methode voor het laten zien van een afbeelding. 
+// De afbeelding word opgehaald van uit het bestand syteem van de computer.
+    $(function () {
     $("input[name=file1]").previewimage({
         div: ".preview",
         imgwidth: 180,
