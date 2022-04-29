06bc0eec4de40c5bc8e701714cd8784d6c4a4fc1
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-29 10:13:26 +0200

    Aanpassingen
    Diensten ophalen met de juiste project erbij

--- backend/class/Diensten.php
+++ backend/class/Diensten.php
@@ -11,8 +11,19 @@ class Diensten extends DbConfig
         $stmt = $this->connect()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
-    }
- 
+    }  
+
+
+    public function getDiensten_Project(){ 
+        $sql = "SELECT * FROM diensten INNER JOIN projecten_diensten on diensten_id = projecten_diensten.diensten_diensten_id AND projecten_diensten.projecten_project_id = 4 ";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    } 
+
+
+
+
  
 
 }
