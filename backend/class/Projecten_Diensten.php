abdc8828bb8f251412ae0f6ad2fae0412984e542
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-29 11:02:35 +0200

    Id invoeren in tabel projecten_diensten

--- backend/class/Projecten_Diensten.php
+++ backend/class/Projecten_Diensten.php
@@ -8,12 +8,12 @@ class Projecten_Diensten extends DbConfig
 {
 
 
-public function Addid($id) { 
-    $sql="";
+public function Addid($project_id,$diensten_id) { 
+    $sql="INSERT INTO projecten_diensten(projecten_project_id,diensten_dienst_id)
+    VALUES($project_id, $diensten_id)";
     $stmt = $this->connect()->prepare($sql);
     $stmt->execute();
-
-
+ 
 }
 
 
