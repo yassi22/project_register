bfc7385f666ae7c5479bf8c04df98df5bef14716
Yassin Chamlal <yassin-c02@hotmail.com>
2022-04-29 10:24:17 +0200

    Code voor ophalen van id van project en diensten

--- backend/class/Projecten_Diensten.php
+++ backend/class/Projecten_Diensten.php
@@ -5,5 +5,15 @@ require_once 'Dbconfig.php';
 class Projecten_Diensten extends DbConfig
 {
 
+
+public function Addid($id) { 
+    $sql="";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute();
+
+
+}
+
+
  
 }
