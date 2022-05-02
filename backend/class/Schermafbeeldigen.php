7c011557f2efb247f588b7c2176a3398247d0c6e
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-02 18:40:47 +0200

    Aanpassingen
    Code toegevoegd voor het invoeren van een schermafbeelding
    
    Code toegevoegd voor het ophalen van scherafbeeldigen

--- backend/class/Schermafbeeldigen.php
+++ backend/class/Schermafbeeldigen.php
@@ -5,10 +5,47 @@ require_once 'Dbconfig.php';
 class Schermafbeelding extends DbConfig{
  
 
+      public function addProject($projectnaam, $datum, $websitelink, $omschrijving, $klantnaam)
+   {
+      try {
+         $sql = "INSERT INTO projecten(projectnaam,datum,website_link,omschrijving,klant_id) 
+          VALUES (:projectnaam,:datum,:websitelink,:omschrijving,:klantnaam)";
+         $db = $this->connect();
+
+         $stmt = $db->prepare($sql);
+         $stmt->bindParam(":projectnaam", $projectnaam);
+         $stmt->bindParam(":datum", $datum);
+         $stmt->bindParam(":websitelink", $websitelink);
+         $stmt->bindParam(":omschrijving", $omschrijving);
+         $stmt->bindParam(":klantnaam", $klantnaam);
+         if ($stmt->execute()) {
+
+            return $db->lastInsertId();
+            //header("Location: overzicht-projecten.php"); 
+
+         } else {
+            throw new Exception("Het invoeren van een schermafbeelding is niet gelukt ");
+         }
+      } catch (Exception $e) {
+         return $e->getMessage();
+      }
+   }
+
+
+    public function getSchermafbeeldigen()
+   {
+      $sql = "SELECT * FROM Schermafbeeldigen";
+      $stmt = $this->connect()->prepare($sql);
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_OBJ);
+   } 
 
 
 
 
 
-    
+ 
+
+
+
 }
