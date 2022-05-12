1139c2f9fc3ac3af4ec71044ab181a6d369dcce7
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-12 10:45:39 +0200

    wit ruimte verwijderd

--- backend/class/Diensten.php
+++ backend/class/Diensten.php
@@ -11,10 +11,11 @@ class Diensten extends DbConfig
         $stmt = $this->connect()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
-    }  
+    }
 
 
-    public function getDiensten_Project(){ 
+    public function getDiensten_Project()
+    {
         $sql = "SELECT * FROM diensten INNER JOIN projecten_diensten on diensten_id = projecten_diensten.diensten_diensten_id AND projecten_diensten.projecten_project_id = 4 ";
         $stmt = $this->connect()->prepare($sql);
         $stmt->execute();
@@ -22,8 +23,5 @@ class Diensten extends DbConfig
     } 
 
 
-
-
- 
-
+    
 }
