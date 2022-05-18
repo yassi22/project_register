b490cc80602145767501fdeb2172503e285e1ec0
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-18 10:45:32 +0200

    Aanpassingen
    Code verplaatst naar verwerk pagina
    Code aangepast

--- backend/class/Schermafbeeldingen.php
+++ backend/class/Schermafbeeldingen.php
@@ -7,27 +7,10 @@ class Schermafbeeldingen extends DbConfig
 {
  
    public function addSchermafbeelding($naamplaatje, $project_id){
-        $naamplaatje = $_FILES['file']['name'];
-        $target_dir = "/img";
-        $target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-        // hier wordt een bestand type geselecteerd
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        // een array om te kijken welke bestand type het is 
-        $extensions_arr = array("jpg", "jpeg", "png", "gif");
-
-        // Check extension
-        if (in_array($imageFileType, $extensions_arr)) {
-        }
-
-        // Upload file
-        move_uploaded_file($_FILES['file']['tmp_name'], $target_dir . $naamplaatje);
-
         try {
             $sql = "INSERT INTO schermafbeeldigen(naam,project_id) 
             VALUES ('" . $naamplaatje . "')";
-            $db = $this->connect();
+            $db = $this->connect(); 
             $stmt = $db->prepare($sql);
             $stmt->bindParam(":file", $naamplaatje);
             if ($stmt->execute()) {
--- handler/projectVerwerk.php
+++ handler/projectVerwerk.php
@@ -23,6 +23,23 @@ if (isset($_POST['addProject'])) {
 
    
     foreach ($naamplaatje as $plaatje) {
+
+        $naamplaatje = $_FILES['file']['name'];
+        $target_dir = "img/";
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+
+        // hier wordt een bestand type geselecteerd
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // een array om te kijken welke bestand type het is 
+        $extensions_arr = array("jpg", "jpeg", "png", "gif");
+
+        // Check extension
+        if (in_array($imageFileType, $extensions_arr)) {
+        }
+
+        // Upload file
+        move_uploaded_file($_FILES['file']['tmp_name'], $target_dir . $naamplaatje);
         $schermafbeeldingenIns->addSchermafbeelding($plaatje, $project_id);
     } 
      
