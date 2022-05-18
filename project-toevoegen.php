99a74259c4d15fcd9d55a2edf96a202885a10d0d
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-18 14:34:42 +0200

    Naam formulier aangepast

--- project-toevoegen.php
+++ project-toevoegen.php
@@ -169,7 +169,7 @@ $categorien =  $categorieIns->getCategorie();
                                         <label for="file-upload" class="custom-file-upload">
                                             Nieuwe schermafbeelding
                                         </label>
-                                        <input type="file" id="file-upload" name="file1"></input>
+                                        <input type="file" id="file-upload" name="bestand"></input>
                                  
                                         <br>
                                         <br>
