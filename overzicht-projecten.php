e36f6776930be6db98c6812035b1cbbadf23db57
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-20 12:12:40 +0200

    Aanpassingen
    Aanpassing voor het zoeken naar projecten

--- overzicht-projecten.php
+++ overzicht-projecten.php
@@ -86,7 +86,7 @@ if (isset($var1)) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="css/style.css">
-  
+
 
   <!-- Load Google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -95,6 +95,7 @@ if (isset($var1)) {
   <script>
     function Search(item) {
       var collection = document.getElementsByClassName("titel");
+      // var collectie = document.getElementByIdByClassName("card-body");
       for (i = 0; i < collection.length; i++) {
         if (((collection[i].innerHTML).toLowerCase()).indexOf(item) > -1) {
           collection[i].style.display = "block";
