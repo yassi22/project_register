fb28d6475be2c7319b956108189806e9ecffa119
Yassin Chamlal <yassin-c02@hotmail.com>
2022-05-27 10:23:48 +0200

    fix plaatjes ophalen

--- project.php
+++ project.php
@@ -184,7 +184,7 @@ $categorieen = $categorieIns->getCategorie_projecten($_GET['id']);
 
                                 <div class="col-sm-4 m-2 ml-2">
                                     <?php foreach ($afbeeldingen as $afbeelding) { ?>
-                                        <a data-fancybox="gallery" data-src='img/"<?php echo $afbeelding->naam ?>"' data-caption="Optional caption,&lt;br /&gt;that can contain &lt;em&gt;HTML&lt;/em&gt; code">
+                                        <a data-fancybox="gallery" data-src='img/<?php echo $afbeelding->naam ?>' data-caption="Optional caption,&lt;br /&gt;that can contain &lt;em&gt;HTML&lt;/em&gt; code">
                                             <img src='img/<?php echo $afbeelding->naam ?>' class="rounded plaatje-grote" /> 
                                      <?php } ?> 
                                 </div>
